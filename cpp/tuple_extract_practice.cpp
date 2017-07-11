ddbbb9465f3c6575c95b904a1f45a06a791913da
kaibasira <super.perl.user@gmail.com>
2017-07-11 10:10:47 +0900

    practice completed.(助けてもらった)

--- cpp/tuple_extract_practice.cpp
+++ cpp/tuple_extract_practice.cpp
@@ -1,32 +1,32 @@
 #include <iostream>
-#include <utility>
+#include <initializer_list>
 #include <tuple>
+#include <type_traits>
+#include <utility>
 
-template<typename T>
-constexpr bool is_even_number_impl_impl(const T& v) {
-	return (v % 2 == 0);
+template<typename Tuple, std::size_t... Indices>
+constexpr bool is_even_number_impl(Tuple&& tup, std::index_sequence<Indices...>) {
+	bool result = true;
+	using swallow = std::initializer_list<int>;
+	(void)swallow { ( void(result &= (std::get<Indices>(tup) % 2 == 0)), 0)... };
+	return result;
 }
 
-template<typename T, typename... Ts>
-constexpr bool is_even_number_impl_impl(const T& v, const Ts&... vs) {
-	return is_even_number_impl_impl(v)
-		&& is_even_number_impl_impl(vs...);
-}
-
-template<typename T, typename Tuple, std::size_t... I>
-constexpr bool is_even_number_impl(const Tuple& t, std::index_sequence<I...>) {
-	return is_even_number_impl_impl(std::get<I>(t)...);
-}
-
-template<typename Tuple,typename I = std::make_index_sequence< std::tuple_size<Tuple>::value >>
-constexpr auto is_even_number(const Tuple& t) {
-	return is_even_number_impl(t, I());
+template<typename Tuple>
+constexpr bool is_even_number(Tuple&& tup) {
+	return is_even_number_impl(
+		std::forward<Tuple>(tup),
+		std::make_index_sequence<std::tuple_size<std::decay_t<Tuple>>::value> {}
+	);
 }
 
 int main() {
-	constexpr std::tuple<int, int, int, int, int> v = { 1,2,3,4,5 };
-	constexpr std::tuple<int, int, int, int, int, int, int> w = { 2,4,6,8,10,12,14 };
-	constexpr auto is_even = is_even_number(v);
-	//constexpr auto is_even2 = is_even_number(w);
+	constexpr std::tuple<int, int, int> v = { 1, 2, 3 };
+	constexpr std::tuple<int, int, int, int, int> w = { 2, 4, 6, 8, 10 };
 
+	constexpr bool is_even1 = is_even_number(v);
+	static_assert(!is_even1, "");
+
+	constexpr bool is_even2 = is_even_number(w);
+	static_assert(is_even2, "");
 }
